0b0cd8a0be950a45c4376b6b8dc04d7455c4b077
LionSeopBong <gkstjq309@gmail.com>
2026-02-20 21:37:09 +0900

    시작화면 목업

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>시작 화면</h1>
-      <Link href="/login">시작하기</Link>
+    <div className="flex justify-center items-center min-h-screen ">
+      <div className="relative w-full min-w-[340px] h-screen">
+        <Image src="/bg/begin.jpg" alt="" fill className="object-cover opacity-70" quality={100} priority />
+        <div className="absolute justify-center top-10 left-0 right-0 flex ">
+          <Image src="/bg/logo1.png" alt="" className="" width={350} height={50} quality={100} priority />
+        </div>
+        <div className="absolute inset-0 flex flex-col items-center justify-end pb-16">
+          <Link href="/login" className="my-5 bg-[#E10600] text-white p-5 rounded-2xl font-bold text-2xl">
+            시작하기
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
