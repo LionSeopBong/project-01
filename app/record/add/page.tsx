8a61e10ec06430a48310c1ac1ce1d34a8b9f0a5b
LionSeopBong <gkstjq309@gmail.com>
2026-03-17 15:28:02 +0900

    페이지 미리생성 방지 코드 추가

--- app/record/add/page.tsx
+++ app/record/add/page.tsx
@@ -1,5 +1,6 @@
 import AddRecordPage from "@/app/record/add/AddRecordPage";
 import { Suspense } from "react";
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
