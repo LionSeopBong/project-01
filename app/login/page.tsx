fc3108ef8a99c24ddd7cda28a7e1cb8627758286
LionSeopBong <gkstjq309@gmail.com>
2026-03-22 22:26:20 +0900

    로그인 버튼 구글 로고 고정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -43,12 +43,13 @@ export default function LoginPage() {
       <button
         onClick={handleLogin}
         disabled={submitting}
-        className="flex items-center gap-3 bg-white text-black font-bold px-9 py-3 rounded-full hover:bg-zinc-200 transition disabled:opacity-50"
+        className="flex items-center gap-3 bg-white text-sm text-black font-bold px-9 py-3 rounded-full hover:bg-zinc-200 transition disabled:opacity-50"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+        <svg className="w-10 h-10 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
           <path d="M564 325.8C564 467.3 467.1 568 324 568C186.8 568 76 457.2 76 320C76 182.8 186.8 72 324 72C390.8 72 447 96.5 490.3 136.9L422.8 201.8C334.5 116.6 170.3 180.6 170.3 320C170.3 406.5 239.4 476.6 324 476.6C422.2 476.6 459 406.2 464.8 369.7L324 369.7L324 284.4L560.1 284.4C562.4 297.1 564 309.3 564 325.8z" />
         </svg>
-        {submitting ? "로그인 중..." : "Google로 시작하기"}
+        {/* {submitting ? "로그인 중..." : "Google로 시작하기"} */}
+        {submitting ? "로그인 중..." : <span className="text-lg text-black font-bold">Google 시작하기</span>}
       </button>
     </main>
   );
